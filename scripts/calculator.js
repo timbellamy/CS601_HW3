c67228f91941aed0172e9479b5c34e7db36ebf76
timothybellamy <timbellamy04@gmail.com>
2023-11-20 21:19:22 -0600

    finished

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,8 +1,13 @@
 const threshold = 50;
 
+// Works with vanilla JS prompt and alert
 function calculator(){
-    let person = prompt("Please enter you name: ");
+    let person = prompt("Please enter you name.");
 
+    while(person === ""){
+        person = prompt("You must enter you name to continue. Please enter you name.");
+    }
+    let checkContinue = "";
 
     if (person){
         alert(`Hi ${person}`);
@@ -10,30 +15,48 @@ function calculator(){
         do {
             let userList =
                 prompt("Please enter a list of numbers separated by a comma.");
+            checkContinue = "";
 
             let numberArray = convert(userList.split(","));
+            if(numberArray.includes(NaN)){
+                alert(`${userList} contians non numbers. Please only enter numbers.`);
+                checkContinue = "yes";
+                continue;
+            }
 
             numberArray = sort(numberArray);
             let total = calculate(numberArray);
             let totalMessage = "The sum of the numbers is: " + total;
 
-            let message = `${totalMessage} \n${total > threshold ? 'That is a bing number.' : 'That is a small number.'}`;
+            let message = `${totalMessage} \n${total > threshold ? 'That is a big number.' : 'That is a small number.'}`;
 
             alert(message);
+            
+            let recivedValidOption = false
 
+            while(!recivedValidOption){
+                checkContinue = prompt("Would you like to calculate another number? Enter yes or no");
+                if(checkContinue === "yes" || checkContinue === "no"){
+                    recivedValidOption = true;
+                }else{
+                    alert(`${checkContinue} is not a valid option`);
+                }
+            }
 
-        }while (checkContinue())
+        }while (checkContinue === "yes");
+
+        alert("Thank you for using Calculator.");
     }
 }
 
-const convert = (list) =>{
+function convert(list){
     let numberArray = [];
 
     list.forEach((num) => { numberArray.push(parseInt(num))});
     return numberArray;
 }
 
-const sort = (numberArray) =>{
+function sort(numberArray){
     let newArray = [];
 
     for(let i = 0; i < numberArray.length; i++){
@@ -49,7 +72,7 @@ const sort = (numberArray) =>{
     return newArray;
 }
 
-const calculate = (numberArray) => {
+function calculate(numberArray){
     let total = 0;
     numberArray.forEach((num) => {
         total += num;
@@ -58,11 +81,27 @@ const calculate = (numberArray) => {
     return total;
 }
 
+const numberValidator =(data) =>{
+    let list = data.split(",");
+    list = convert(list);
+    if(list.includes(NaN)){
+        return `${data} contains non numbers.`;
+    }
+    return "";
+}
+
+const optionValidator = (data) =>{
+    if(data === "yes" || data === "no"){
+        return "";
+    }
+    return "Please enter yes or no.";
+}
+
 
 async function run(){
     let message = "Hi!, what is your name?";
     // Create a prompt to get the user's name. This should wait for the results.
-    let userName = await createPromt(message, "Submit", "Cancel");
+    let userName = await createPrompt(message, "Submit", "Cancel");
 
     // Display username
     await createAlert(`Hi! ${userName}`);
@@ -71,7 +110,7 @@ async function run(){
         // Create a prompt to get the user to enter in a list of numbers.
         //The program will pause for results;
         message = "Please enter a list of numbers separated by a comma.";
-        let userList = await createPromt(message, "Submit", "Cancel");
+        let userList = await createPrompt(message, "Submit", "Cancel", numberValidator);
 
         // Parse the recived list to get numbers
         let numberArray = convert(userList.split(","));
@@ -85,11 +124,11 @@ async function run(){
         //Display to the user the total and if the value is large or small (large > 50)
         let totalMessage = "The sum of the numbers is: " + total;
 
-        message = `${totalMessage} \n${total > threshold ? 'That is a bing number.' : 'That is a small number.'}`;
+        message = `${totalMessage} \n${total > threshold ? 'That is a big number.' : 'That is a small number.'}`;
         await createAlert(message);
         // Prompt use to see if they would like to use the calculator again. Pause for return value;
         message = "Would you like to calculate another number? Enter yes or no"
-        let checkContinue = await createPromt(message, "Submit", "Cancel");
+        let checkContinue = await createPrompt(message, "Submit", "Cancel", optionValidator);
         calculateAnother = checkContinue === "yes";
     }while(calculateAnother);
     
@@ -98,7 +137,7 @@ async function run(){
 }
 
 function runCalculator(){
-    let data = createPromt("Please enter a list of numbers separated by a comma.", "Submit",  "Canel");
+    let data = createPrompt("Please enter a list of numbers separated by a comma.", "Submit",  "Canel");
 
 }
 
@@ -123,8 +162,8 @@ function createModal(message){
     return modalContent;
 }
 
-function createPromt(message, submittBtnText, cancelBtnText){
-    return new Promise((resolve, reject) =>{
+function createPrompt(message, submittBtnText, cancelBtnText, validator){
+    return new Promise((resolve) =>{
         let doc = document;
         let modalContent = createModal(message);
 
@@ -137,6 +176,10 @@ function createPromt(message, submittBtnText, cancelBtnText){
         inputBox.type = Text;
         inputBox.id = "input"
         inputBox.className = "input"
+
+        let errorMessage = inputDiv.appendChild(doc.createElement("div"));
+        errorMessage.id = "error-message";
+        errorMessage.className = "error-message";
     
         let btnGroup = modalContent.appendChild(doc.createElement("div"));
         btnGroup.className = "btn-group";
@@ -158,7 +201,10 @@ function createPromt(message, submittBtnText, cancelBtnText){
         submitBtn.classList.add("submit-btn", "btn");
         submitBtn.type = "submit";
         submitBtn.onclick = function (){
-           resolve(onSubmit(document.getElementById("input").value));
+            let data = onSubmit(document.getElementById("input").value, validator);
+            if(data !== undefined){
+                resolve(data);
+            }
            }
     });
 }
@@ -184,14 +230,16 @@ function createAlert(message){
 
 }
 
-function onSubmit(data){
-    if(data === "" && document.getElementById("error-message") === null){
-        let errorMessage = document.getElementById("input-div").appendChild(document.createElement("div"));
-        errorMessage.id = "error-message";
-        errorMessage.className = "error-message";
-        errorMessage.innerHTML = "This field is requried.";
-    }
-    if(data !== ""){
+function onSubmit(data, validator){
+    let validatorMessage = validator !== undefined ? validator(data) : "";
+
+    if(data === ""){
+        document.getElementById("error-message").innerHTML = "This field is requried.";
+    }else if(validatorMessage !== ""){
+        document.getElementById("error-message").innerHTML = validatorMessage;
+        // Clear input box to keep from advancing.
+        document.getElementById("input").value = "";
+    }else{
         document.getElementById("container").removeChild(document.getElementById("modal-container"));
         return data;
     }
